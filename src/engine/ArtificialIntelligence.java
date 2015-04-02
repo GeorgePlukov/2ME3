456b2920900790e6651119ffc302cc442e60b378
philip-h <philip.habib6347@gmail.com>
2015-04-02 13:48:30 -0400

    Commented the AI class

--- src/engine/ArtificialIntelligence.java
+++ src/engine/ArtificialIntelligence.java
@@ -9,76 +9,82 @@ public class ArtificialIntelligence {
 	
 	public static int getMove(Chip[][] chips)
 	{
-		int col = -1337;
-		
+		int col;
+		//Check to see if the AI can win in one move, if it can, take it!
 		col = checkAIWin(chips);
-		System.out.println("AI WIN: "+col);
 		if (col != -1)
 			return col;
-		
+		//Check to see if the opponent can win in one move, if it can, take that move
 		col = checkPlayerWin(chips);
-		System.out.println("PLAY WIN: "+col);
 		if (col != -1)
 			return col;
 		
-//		//Go in center if no one else has
-//		if (chips[3][0] == null)
-//			return 3;
-//		
-//		//Go in the center of the board, not the edges
-//		if (!centerFull(chips)) 
-//			return (int)(Math.random()*5)+1;
-//		
-//		
-//		//If one edge is full, go in the other
-//		if (chips[0][5] != null)
-//			return 6;
-//		
-//		if (chips[6][5] != null)
-//			return 0;
-//		
-//		// If center is full, but edges are not, go in a random edge
-//		System.out.println("Random edge");
-//		return (Math.random() < 0.5) ? 0 : 6;
-		return (int)(Math.random()*7);
-		
+		//Go in center if no one else has
+		if (chips[3][0] == null)
+			return 3;
+
+		//Go in the center of the board, not the edges
+		if (!centerFull(chips))
+			return (int)(Math.random()*5)+1;
+
+
+		//If one edge is full, go in the other
+		if (chips[0][5] != null)
+			return 6;
+
+		if (chips[6][5] != null)
+			return 0;
+
+		// If center is full, but edges are not, go in a random edge
+		return (Math.random() < 0.5) ? 0 : 6;
 	}
 
 	private static int checkAIWin(Chip[][] chips)
 	{
-		int col = -1337;
+		int col;
+		//Check if there are 3 pieces in a horizontal line
 		col = checkHorizontal(chips,Color.BLUE);
 		if (col != 1)
 			return col;
+		//Check if there are 3 pieces in a vertical line
 		col = checkVertical(chips, Color.BLUE);
 		if (col != -1)
 			return col;
+		//Check if there are 3 pieces in an upward diagonal line
 		col = checkDiagonalUp(chips, Color.BLUE);
 		if (col != -1)
 			return col;
+		//Check if there ar 3 pieces in a downward diagonal line
 		col = checkDiagonalDown(chips, Color.BLUE);
 		if (col != -1)
 			return col;
+		//Return -1 if there are no one move wins
 		return -1;
 	}
 	
 	private static int checkPlayerWin(Chip[][] chips) 
 	{
-		int col = -1337;
+		int col;
+		//Check if there are 3 pieces in a horizontal line
 		col = checkHorizontal(chips, Color.RED);
 		if (col != -1)
 			return col;
+		//Check if there are 3 pieces in a vertical line
 		col = checkVertical(chips,Color.RED);
 		if (col != -1)
 			return col;
+		//Check if there are 3 pieces in an upward diagonal line
 		col = checkDiagonalUp(chips, Color.RED);
 		if (col != -1)
 			return col;
+		//Check if there ar 3 pieces in a downward diagonal line
 		col = checkDiagonalDown(chips, Color.RED);
 		if (col != -1)
 			return col;
+		//Return -1 if there are no one move blocks (block as in blocking an opponent win)
 		return -1;
 	}
+
 	/*This checks to see if there are 3 pieces of the same color lined up in a horizontal fashion. 
 	 * If there are, whether or not they are my color, I go there*/
 	private static int checkHorizontal(Chip[][] chips, Color c)
@@ -87,7 +93,7 @@ public class ArtificialIntelligence {
 		{
 			for (int i = 0; i < chips.length - 3; i++)
 			{
-				
+				//Add all 4 piece horizontal lines to an array list (one line at a time)
 				ArrayList<Color> colors = new ArrayList<>();
 				colors.add(chips[i+0][j] == null ? null : chips[i+0][j].getColor());
 				colors.add(chips[i+1][j] == null ? null : chips[i+1][j].getColor());
@@ -103,6 +109,7 @@ public class ArtificialIntelligence {
 				}
 			}
 		}
+		//No horizontal win
 		return -1;
 	}
 	
@@ -160,6 +167,7 @@ public class ArtificialIntelligence {
 		{
 			for (int i = 0; i < chips.length-3; i++)
 			{
+				//Add all 4 piece upward diagonal lines to an array list (one line at a time)
 				ArrayList<Color> colors = new ArrayList<>();
 				colors.add(chips[i+0][j+0] == null ? null : chips[i+0][j+0].getColor());
 				colors.add(chips[i+1][j+1] == null ? null : chips[i+1][j+1].getColor());
@@ -177,6 +185,7 @@ public class ArtificialIntelligence {
 			}
 
 		}
+		//No upward diagonal win
 		return -1;
 	}
 	
@@ -185,7 +194,8 @@ public class ArtificialIntelligence {
 		for (int j =  chips[0].length-1; j >= 3; j--)
 		{
 			for (int i = 0; i < chips.length-3; i++)
-			{	
+			{
+				//Add all 4 piece upward diagonal lines to an array list (one line at a time)
 				ArrayList<Color> colors = new ArrayList<>();
 				colors.add(chips[i+0][j-0] == null ? null : chips[i+0][j-0].getColor());
 				colors.add(chips[i+1][j-1] == null ? null : chips[i+1][j-1].getColor());
@@ -203,9 +213,11 @@ public class ArtificialIntelligence {
 			}
 
 		}
+		//No downward diagonal win
 		return -1;
 	}
-	
+	/*This method checks to see if the center of the board is full by checking if the tops of the center cols are null
+	* If all of the chips at [i][5] are null then the center cannot be full*/
 	private static boolean centerFull(Chip[][] chips)
 	{
 		for (int i = 1; i < chips.length-1; i++)
